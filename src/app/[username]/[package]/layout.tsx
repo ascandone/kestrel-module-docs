52eac658b9afc95bbc3b4f1ee4dddebe6fcaae6f
Alessandro Scandone <alescandone@gmail.com>
2024-03-16 01:45:30 +0100

    better ui

--- src/app/[username]/[package]/layout.tsx
+++ src/app/[username]/[package]/layout.tsx
@@ -1,26 +1,54 @@
 import { ModulesSideBar } from "@/components/ModulesSidebar";
-import { ReactNode } from "react";
+import Link from "next/link";
+import { FC, ReactNode } from "react";
+import { Bars3Icon } from "@heroicons/react/24/outline";
 
 type Params = {
   username: string;
   package: string;
 };
 
-export default function Layout({
+const TopMenu: FC<{ params: Params }> = ({
+  params: { username, package: package_ },
+}) => (
+  <div className="md:hidden px-4 py-2 border shadow-sm flex gap-x-4 items-center justify-between">
+    <h3>
+      <Link
+        href={`/${username}/${package_}`}
+        className="text-pink-800 font-semibold text-mono"
+      >
+        {package_}
+      </Link>
+    </h3>
+
+    <Bars3Icon className="h-6" />
+  </div>
+);
+export default async function Layout({
   params: { package: package_, username },
   children,
 }: {
   params: Params;
   children: ReactNode;
 }) {
+  const json = await fetch(
+    `https://raw.githubusercontent.com/${username}/${package_}/main/docs.json`
+  ).then((r) => r.json());
+
+  const modules = Object.keys(json.modules).sort();
+
   return (
-    <div className="px-4">
-      <div className="grid grid-cols-12 gap-x-4">
-        <div className="col-span-3">
-          <ModulesSideBar package_={package_} username={username} />
-        </div>
-        <main className="col-span-8">{children}</main>
+    <>
+      <TopMenu params={{ username, package: package_ }} />
+
+      <div className="fixed w-60 transition-transform duration-200 ease-in-out -translate-x-full md:-translate-x-0">
+        <ModulesSideBar
+          modules={modules}
+          package_={package_}
+          username={username}
+        />
       </div>
-    </div>
+      <main className="col-span-8 md:ml-64 px-6 py-2">{children}</main>
+    </>
   );
 }
--- src/components/ModulesSidebar.tsx
+++ src/components/ModulesSidebar.tsx
@@ -1,44 +1,33 @@
 import { FC } from "react";
-import { usePathname } from "next/navigation";
 import Link from "next/link";
 
 export const ModulesSideBar: FC<{
   username: string;
   package_: string;
-}> = async ({ username, package_ }) => {
-  const json = await fetch(
-    `https://raw.githubusercontent.com/${username}/${package_}/main/docs.json`
-  ).then((r) => r.json());
-
-  const modules = Object.keys(json.modules).sort();
-
-  return (
-    <div className="py-4">
-      <h3>
-        <Link
-          href={`/${username}/${package_}`}
-          className="text-pink-800 font-semibold text-mono"
-        >
-          {package_}
-        </Link>
-      </h3>
-
-      <div className="h-4"></div>
-
-      <h2 className="text-black font-bold">Modules:</h2>
-
-      <ul className="list-disc list-inside">
-        {modules.map((module) => (
-          <li key={module}>
-            <Link
-              href={`/${username}/${package_}/${module}`}
-              className="text-pink-800 font-semibold text-mono"
-            >
-              {module}
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
-};
+  modules: string[];
+}> = async ({ modules, username, package_ }) => (
+  <div className="py-4 px-4 overflow-y-auto h-screen bg-white border shadow _md:shadow-none">
+    <h3>
+      <Link
+        href={`/${username}/${package_}`}
+        className="text-pink-800 font-semibold text-mono"
+      >
+        {package_}
+      </Link>
+    </h3>
+    <div className="h-4"></div>
+    <h2 className="font-bold text-gray-900 text-lg">Modules</h2>
+    <ul>
+      {modules.map((module) => (
+        <li key={module}>
+          <Link
+            href={`/${username}/${package_}/${module}`}
+            className="text-pink-800 font-semibold text-mono"
+          >
+            {module}
+          </Link>
+        </li>
+      ))}
+    </ul>
+  </div>
+);
