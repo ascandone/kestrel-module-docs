d33ef2d3578ee039922f995045c7a7a7e0f77915
Alessandro Scandone <alescandone@gmail.com>
2025-08-30 09:43:43 +0200

    give more room to type sigs

--- src/app/[username]/[package]/[...namespace]/page.tsx
+++ src/app/[username]/[package]/[...namespace]/page.tsx
@@ -15,8 +15,10 @@ const ItemCard: FC<{
       <h2
         className={`
         text-pink-800 font-sans font-medium
-          pl-2 py-1 rounded-sm border-l-4 border-pink-800
-          bg-gradient-to-r from-pink-200 to-pink-50/10
+          pl-2 py-2 md:rounded-sm md:border-l-4 border-pink-800
+          bg-pink-200
+
+          -mx-3 md:mx-0
         `}
       >
         <pre className="whitespace-pre-wrap">{children}</pre>
